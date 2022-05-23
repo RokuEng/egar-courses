35961403f434ba209fba3b738b755454411dc92f
RokuEng <alexandr.stenkin@gmail.com>
2022-05-23 18:53:21 +0300

    ...

--- src/main/java/com/RokuEng/les4/core/domain/item/armor/Helmet.java
+++
@@ -1,9 +0,0 @@
-package com.RokuEng.les4.core.domain.item.armor;
-
-public class Helmet implements Protectable {
-
-    @Override
-    public int protect() {
-        return 10;
-    }
-}
--- src/main/java/com/RokuEng/les4/core/domain/item/armor/Protectable.java
+++
@@ -1,6 +0,0 @@
-package com.RokuEng.les4.core.domain.item.armor;
-
-public interface Protectable {
-    int protect();
-
-}
